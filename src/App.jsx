4d94a3628f3b544ca21d712a0366ca8f50cf1222
Aniyo44 <yaf_did@outlook.com>
2024-02-16 10:48:45 +0100

    pushing to git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <div className='center'>
+     <p className='title'> DevOps <span style={{color:"white"}}>Porject</span> </p>
+     </div>
+    </>
+  )
+}
+
+export default App
